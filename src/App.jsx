e1a909ca47c0eb92f787ca213857b43ce5c12af9
Shinjan Patra <srikpatra.2008@gmail.com>
2024-01-16 00:48:09 +0530

    basic changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import "./App.scss";
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contacts</section>
+  </div>;
 };
 
 export default App;
